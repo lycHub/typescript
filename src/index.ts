5b68ded01bcc40eb8ae7444fe4e7f58d4beff770
lycHub <1120805364@qq.com>
2019-04-25 20:01:06 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const num: number = 111;
+function identity(num: number): number {
+  return num;
+}
+
+console.log('num', identity(num));
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+  module.exports = {
+    entry: './src/index.ts',
+    output: {
+      filename: 'bundle.js',
+      path: resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      hot: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Hi TypeScript',
+        // template: '../index.html',
+        template: resolve(__dirname, 'src/index.html'),
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ]
+    }
+  };
