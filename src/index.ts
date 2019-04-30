db042c17f5257b10cecb1cd787fbc0fbe28c0c56
lycHub <1120805364@qq.com>
2019-04-30 20:05:55 +0800

    高级类型

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-window.onmousedown = function(event: MouseEvent) {
-console.log('mouseEvent :', event.clientX);
+function mergeFuncs<T, U>(arg1: T, arg2: U): T | U {
+  let res = {} as T & U;
+  res = { ...arg1, ...arg2 };
+  return res;
 }
